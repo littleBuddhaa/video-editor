d009e408e3fa404d42477799956ee65cb9c36a22
littleBuddhaa <somineegupta@gmail.com>
2019-04-01 05:22:32 +0530

    cropping func

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask, render_template,flash, request, redirect, url_for, session, request,logging
 import urllib.request
+import subprocess
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.config import get_setting
+import datetime
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+session_url = ''
 
 @app.route('/')
 def index():
+    subprocess.run(['rm' , 'static/cropped/new.webm'])
+    subprocess.run(['rm' , 'static/uploads/mysample.webm'])
 
 
     return render_template('home.html')
@@ -17,14 +24,40 @@ def search():
     if request.method == 'POST':
         url = (request.form['url'])
         print ('Downloading Started..')
-        up = 'fetching failed'
-        name  = 'uploads/mysample.webm'
+        up = 0
+        name  = 'static/uploads/mysample.webm'
+        session_url = name
         urllib.request.urlretrieve(url, name)
         print ('Download Completed!')
-        up = 'fetched succesfully'
-
+        up = 1
+        dur = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', name])
+        print (dur)
         
-    return render_template('home.html',var = up)
+    return render_template('home.html',var = up,url = url,dur = str(dur))
+@app.route('/crop',methods = ['GET','POST'])
+def crop():
+    if request.method == 'POST':
+        start = (request.form['start'])
+        end  = (request.form['end'])
+        print ('user entered from (%f) to (%f)',(start,end))
+        hrst = (datetime.timedelta(seconds=int(start)))
+        hrend = (datetime.timedelta(seconds=int(end)))
+        crurl = '../static/cropped/new.webm'
+        try:
+            print('******************************')
+            print(session_url)
+            #subprocess.run(['ffmpeg_extract_subclip(', session_url ,',', start,',', end,',', 'targetname="/downloads/cropped.webm")'])
+            #subprocess.run(['ffmpeg_extract_subclip( session_url, start, end, targetname="/downloads/cropped.webm")'])
+            #subprocess.run(['ffmpeg', '-i' , 'uploads/mysample.webm', '-ss' ,  str(hrst), '-to' , str(hrend), '-c:v' ,  'copy' , '-c:a' , 'copy' ,  'cropped/new.webm'])
+            subprocess.run(['ffmpeg', '-i' , 'static/uploads/mysample.webm', '-ss' ,  str(hrst), '-to' , str(hrend), 'static/cropped/new.webm'])
+
+            err = 0
+        except subprocess.CalledProcessError as e:
+            print (e.output)
+            err = 1
+
+
+    return render_template('home.html',err = err,crurl = crurl)
 
     
 
